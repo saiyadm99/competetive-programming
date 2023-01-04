a9da21a30e09b161e226d8adbce75506d3846d0e
saiyadm99 <81185811+saiyadm99@users.noreply.github.com>
2023-01-04 13:38:06 +0600

    Too lazy to name the question
    https://www.hackerearth.com/problem/algorithm/too-lazy-to-name-the-question/

---
+++ hackerearth/Too lazy to name the question.cpp
@@ -0,0 +1,37 @@
+// https://www.hackerearth.com/problem/algorithm/too-lazy-to-name-the-question/
+//#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a , b, c;
+  cin >> a >> b >> c;
+  int cth_num;
+  int j = 0;
+  for(int i=2; j<c; i++){
+    if(i%a == 0 || i%b == 0){
+      cth_num = i;
+      j++;
+    }  
+  }
+
+  int lpc;
+  for(int i=0; i<=a*b; i++){
+    if(i%a == 0 && i%b == 0){
+      lpc = i;
+    }
+  }
+
+  int step;
+  if(cth_num % a == 0 && cth_num % b == 0){
+    step = lpc;
+  }else if(cth_num % a == 0){
+    step = a;
+  }else{
+    step = b;
+  }
+
+  for(int i = cth_num; i>=0; i = i-step){
+    cout << i << " ";
+  }
+}
